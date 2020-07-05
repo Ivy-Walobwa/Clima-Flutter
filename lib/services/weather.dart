fb4a5f0663b605fa35614c68038f09087d3b826d
Ivy Walobwa <ivywalobwa@gmail.com>
2020-07-05 10:52:07 +0300

    project complete

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,30 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
 class WeatherModel {
+
+  final apiKey = '711aa1dac24cc43c62a34ea3132602de';
+  final openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+  Future<dynamic> getLocationWeather() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper =  NetworkHelper(url:
+    '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getCityWeather(cityName) async{
+    NetworkHelper networkHelper =  NetworkHelper(url:
+    '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
